cc77df0c7f41ee712bfb3ac6b742a24a1081cc8e
eduardin games <eduardo.pcavalcante@eaportal.org>
2024-09-12 13:58:11 +0000

    omençano o começo 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,8 @@ import moeda from "../assets/adesivo_moeda.svg"
 import quadrados from "../assets/adesivo_quadrados.svg"
 import china from "../assets/china.svg"
 import seta_direita from "../assets/seta_direita.svg"
+
+
 import x from "../assets/x.svg"
 
 export default function Login() {
@@ -42,15 +44,12 @@ export default function Login() {
             <div className="relative ">
                 <h1 className="flex justify-center mb-10 text-[64px] font-bold mx-[700px] text-center text-white">Filmes, séries e muito mais, sem limites</h1>
                 <h3 className="flex justify-center mb-5 text-[24px] font-semibold text-white">A partir de R$ 20,90. Cancele quando quiser.</h3>
-                <p className="flex justify-center text-[18px] text-white">Quer assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+                <p className="flex justify-center text-[18px] text-white ">Quer assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
 
                 <div className="flex justify-center mt-4">
                     <input className="px-32 mr-2 rounded-[4px] opacity-75 bg-gray-900 py-4 border-2 border-neutral-500" type="Email" />
                     <div>
-                        <button className="flex bg-red-600 border-[1px] border-red-600 px-8 py-[10px] rounded-[3px] hover:text-gray-100  hover:bg-red-800 text-white font-semibold">
-                            <p className="text-[24px]">Vamos lá  </p>
-                            <img className="w-24" src={seta_direita} alt="seta para esquerda" />
-                        </button>
+                        <button className="flex bg-red-600 border-[1px] border-red-600 px-8 py-[10px] rounded-[3px] hover:text-gray-100  hover:bg-red-800 text-white font-semibold">Vamos lá</button>
                         
                     </div>
                 </div>
@@ -61,16 +60,16 @@ export default function Login() {
         
         <div className="justify-center col-1 pb-96 mt-96">
 
-            <div className=" flex justify-between items-center px-72 text-white"> 
+            <div className=" flex justify-center items-center px-72 text-white group "> 
                 <img className=" w-32"src={pipoca} alt="pipoca" />
-                <div className="bg-gradient-to-r from-purple-950 via-purple-950 to-indigo-950 px-10 py-4 flex rounded-[10px]">
+                <div className="bg-gradient-to-r from-purple-950 via-indigo-950 to-indigo-950 px-10 py-4 flex rounded-[20px] hover:px-20 hover:py-5 hover:bg-gradient-to-r hover:from-purple-900 hover:via-indigo-900 hover:to-indigo-950  hover:rounded[24px] hover:ease-in duration-300">
                     <div className="group">
-                        <h3 className="text-[32px]">A Netflix que você adora por apenas R$ 20,90.</h3>
-                        <p className="text-[20px]">Aproveite nossa opção mais acessível, o plano com anúncios.</p>
+                        <h3 className="text-[28px] font-semibold">A Netflix que você adora por apenas R$ 20,90.</h3>
+                        <p className="text-[24px] ">Aproveite nossa opção mais acessível, o plano com anúncios.</p>
                     </div>
                     
                     <div>
-                        <button className="text-[24px] bg-gray-700 px-10 mx-6 py-2 mt-4 ml-48 rounded-[10px] hover:bg-gray-900">Saiba mais</button>
+                        <button className="text-[24px] bg-neutral-700 px-10 mx-6 py-2 mt-2 ml-48 rounded-[10px] hover:bg-neutral-800">Saiba mais</button>
                     </div>
                 </div>
                 
@@ -126,7 +125,7 @@ export default function Login() {
 
             <section className="mx-64">
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">O que é Netflix?</h2>
                     <img src={x} alt="um x" />
@@ -134,14 +133,14 @@ export default function Login() {
             </div>
 
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">Quanto custa a Netflix?</h2>
                     <img src={x} alt="um x" />
                 </div>
             </div>
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">Onde posso assistir?</h2>
                     <img src={x} alt="um x" />
@@ -149,7 +148,7 @@ export default function Login() {
             </div>
 
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">Como faço para cancelar?</h2>
                     <img src={x} alt="um x" />
@@ -157,7 +156,7 @@ export default function Login() {
             </div>
 
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">O que eu posso assistir na Netflix?</h2>
                     <img src={x} alt="um x" />
@@ -165,7 +164,7 @@ export default function Login() {
             </div>
             
 
-            <div className="bg-neutral-600 mb-4">
+            <div className="bg-neutral-600 mb-4 hover:bg-neutral-500">
                 <div className="flex items-center justify-between">
                     <h2 className="text-white text-[32px] font-semibold ml-10">A Netflix é adequada para crianças?</h2>
                     <img src={x} alt="um x" />
@@ -193,7 +192,7 @@ export default function Login() {
             </section>
 
 
-
+            <botton/>
 
         </div>
     </div>
